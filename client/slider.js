305ce27cc3a1d32c269edc2b748d0e836b638810
Dana Ottaviani <dana.ottaviani@gmail.com>
2016-08-07 15:13:19 -0400

    adding sliding function to piece

--- client/slider.js
+++ client/slider.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 
-	$("#part-1").draggable();
+	//$("#part-1").draggable();
 	$("#part-1").draggable({ snap: "#box" });
 
+	$("#part-1").click(function(){
 
-	$("#part-2").draggable();
+		$(this).animate({ "left": "+=50px" }, "slow" );
+
+	});
+
+	//$("#part-2").draggable();
 		
 });
