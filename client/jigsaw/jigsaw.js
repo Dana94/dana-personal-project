72d9acc8362d2be1fc4635ae22976c6e59f98ffc
Dana Ottaviani <dana.ottaviani@gmail.com>
2016-08-07 18:06:14 -0400

    trying to keep pieces snapped inside the container and to other pieces

--- client/jigsaw/jigsaw.js
+++ client/jigsaw/jigsaw.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 
-		$("#part-1").draggable();
-		$("#part-2").draggable();
-		//$(".piece").draggable();	
+	$("#part-1").draggable({ snap: "#part-2", snapMode: "outer" });
+	//$("#part-1").draggable({ snap: "#box", snapMode: "inner" });
+	//,{ containment: "#box", scroll: false });
+		
+	$("#part-2").draggable({ snap: "#box", snapMode: "inner" });
+
+		//changes z-index of whichever piece is being used inside the div
+		$( "#box div" ).draggable({ stack: "#box div" });
+			
 
 });
 
--- client/slider.js
+++ client/slider.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
 	//$("#part-1").draggable();
-	$("#part-1").draggable({ snap: "#box" });
+	$("#part-1").draggable({ snap: "#box" },{ containment: "#box", scroll: false });
 
 	$("#part-1").click(function(){
 
-		$(this).animate({ "left": "+=50px" }, "slow" );
+		$(this).animate({ "left": "+=50px" }, "fast");
 
 	});
 
